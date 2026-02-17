4c3c18734438dc2fdb96f24f046915e9debdf3e6
DavidG0m3z <david.ardilagomez@gmail.com>
2026-02-17 16:36:51 -0500

    primer commit

---
+++ reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,12 @@
+const firtName : string = "David";
+const lasatName : string = "Gomez";
+
+let diceNumber = 5
+
+console.log(firtName, lasatName);
+
+const containsLetterD = lasatName.includes('D');
+console.log({containsLetterD});
+
+const fullName = `el nombre es: ${firtName} ${lasatName}`;
+console.log({fullName});
